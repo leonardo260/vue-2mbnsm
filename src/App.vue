7b5d0d36c98292cc940855de39e2a48034c5672a
leonardo260 <117398132+leonardo260@users.noreply.github.com>
2022-11-08 07:54:22 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    
+    <HelloWorld msg="grafica de barras"/>
+    
+
+  </div>
+  <div id="app1">
+    
+    
+    
+
+  </div>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
